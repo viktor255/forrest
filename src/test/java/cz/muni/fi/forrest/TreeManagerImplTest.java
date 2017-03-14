56ec43be90dcb0ceb48cd3633610bd667b1d577c
Jakub Bohoš <xbohos@fi.muni.cz>
2017-03-14 22:45:50 +0100

    add TreeManager, TreeManagerImpl,

---
+++ src/test/java/cz/muni/fi/forrest/TreeManagerImplTest.java
@@ -0,0 +1,42 @@
+package cz.muni.fi.forrest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Jakub Bohos 422419
+ */
+public class TreeManagerImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void createTree() throws Exception {
+
+    }
+
+    @Test
+    public void getTree() throws Exception {
+
+    }
+
+    @Test
+    public void updateTree() throws Exception {
+
+    }
+
+    @Test
+    public void deleteTree() throws Exception {
+
+    }
+
+    @Test
+    public void findAllTrees() throws Exception {
+
+    }
+
+}
