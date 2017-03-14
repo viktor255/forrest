b48aba1d278685d623ef601b59ebbf4695d15749
vikto <viktor.lehotsky@gmail.com>
2017-03-14 22:43:55 +0100

    junit add

---
+++ src/test/java/cz/muni/fi/forrest/PotManagerImplTest.java
@@ -0,0 +1,42 @@
+package cz.muni.fi.forrest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vikto on 14.03.2017.
+ */
+public class PotManagerImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void createPot() throws Exception {
+
+    }
+
+    @Test
+    public void updatePot() throws Exception {
+
+    }
+
+    @Test
+    public void deletePot() throws Exception {
+
+    }
+
+    @Test
+    public void findAllPots() throws Exception {
+
+    }
+
+    @Test
+    public void findPotById() throws Exception {
+
+    }
+
+}
